cb47b5899de23b3e0cb2e50fa394d77e6e3e5cdd
Colin McKean <ezikill@hotmail.com>
2017-09-10 17:36:48 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -8,6 +8,6 @@ class EmailParser
     @emails = emails
   end
   def parse
-    @emails.split(" ").map {|email| email.split(", ") }flatten.uniq.
+    @emails.split(" ").map {|email| email.split(", ") }.flatten.uniq
   end
 end
